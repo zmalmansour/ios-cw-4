6527757ed935776f673951afaa778a33051378be
SafeyahShemali <safonorthly@gmail.com>
2020-06-08 03:40:01 +0300

    FirstComment
    Class work is done 😬

---
+++ iOS_Classwork_Extra/iOS_Classwork_Extra/Classwork/Student.swift
@@ -0,0 +1,25 @@
+//
+//  Student.swift
+//  RandomStudent
+//
+//  Created by Safeyah on 6/2/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import Foundation
+
+//⑶📌:  انشئ هيكل باسم Studentو يحتوي على ثلاث خصائص كالتالي
+//name -> String
+//track -> String
+//gender -> String
+
+//⑷📌:  انشئ دالة كينونية تقوم بمقارنة الـ gender …
+//اذا كان طالب  "boy" سيتم ارجاع صورة لها اسم boyProfileImage
+//اذا كانت طالبة  "girl" سيتم ارجاع صورة لها اسم girlProfileImage
+//لترى الصور ستجدها في ملف Assest.xcasswts
+
+//⑸📌:اذا انتهيت من الخطوات السابقة ، عد الى ملف RandomStudent.swift
+
+
+
+
