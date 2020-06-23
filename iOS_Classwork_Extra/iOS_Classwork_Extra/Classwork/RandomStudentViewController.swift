e80dac3fce1d48220f0a0312f4409cdaec00ecf8
Omar <omar@barmej.com>
2020-06-24 02:05:32 +0300

    Great Dietgit add .

--- iOS_Classwork_Extra/iOS_Classwork_Extra/Classwork/RandomStudentViewController.swift
+++ iOS_Classwork_Extra/iOS_Classwork_Extra/Classwork/RandomStudentViewController.swift
@@ -7,56 +7,27 @@
 //
 
 import UIKit
-
+//Do Not touch These⛔️😅⛔️لا تلمس الأسطر البرمجية لهذا الملف
 
 class RandomStudentViewController: UIViewController {
-    
-    var studentName :[String] = [ "محمد علي" , "صالح شهاب" , "ريم خالد " ]
-    var studentTrack :[String]  = ["iOS" , "Web" , "Andriod"]
-    
-    //⑴📌: إلق نظرة سريعة على الكود ثم جرب التطبيق بالضغط على زر (▶️)
-   
-
-    //⑵📌:  توجه الى Student.swift الموجود في ملف Classwork
-
-/*
-     ⑹📌: كل ما تحتاج فعله هو …. ان تستبدل المصفوفات بمصفوفة واحدة و عناصرها هي من هيكل Student ، قم بتعبئة المصفوفة بثلاث كوائن
-    
-    //مثال على كائن :                                                 .
-          Student(name: “صالح شهاب”, track: “iOS”, gender: “boy”)
-
-*/
-    
-//Do Not touch These⛔️😅⛔️لا تلمس الأسطر البرمجية التالية
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var trackLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    //ـــــــــــــــــــــــــــ⛔️😅⛔️ـــــــــــــــــــــــــ//
-    
-    
-    
     //Function that activate when the button is tapped هذه الدالة 👇🏻 التي تعمل عندما يتم الضغط على زر (اختر)
     @IBAction func tappedButton(_ sender: UIButton) {
         
-        let randStudent = arc4random_uniform(UInt32(studentName.count))
+        let randomStudent = students.randomElement()!
         
-         //⑺📌: غير السطرين التالين بحيث ان تستخدم  المصفوفة الجديدة المكونة من  كوائن الـ Student
+        let randomStudentName = randomStudent.name
+        let randomStudentTrack = randomStudent.track
+                
+        self.nameLabel.text = randomStudentName
+        self.trackLabel.text =  randomStudentTrack
         
-        self.nameLabel.text = studentName[Int(randStudent)]
-        self.trackLabel.text =  "مسار الـ " + studentTrack[Int(randStudent)]
-        
-/*
-         ⑻📌:  ازل( // ) عن السطر (58)
-         
-ثم استبدل “🌅” باسم الصورة المفترض ان تظهر …. استخدم الدالة الكينونية 😉
-*/
-        
-        //self.profileImageView = UIImage(named: "🌅")
-        
+        // MARK:   (8) أزل الملاحظة عن الكودالتالي كي تعمل الصور
+         self.profileImageView.image = UIImage(named: randomStudent.imageName())
     }
-    
 }
+
+//ـــــــــــــــــــــــــــ⛔️😅⛔️ـــــــــــــــــــــــــ//
